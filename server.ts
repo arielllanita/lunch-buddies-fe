beb423919c7096e1595bab404f03447ba4ade1b0
Ariel Llanita <llanitaariel@gmail.com>
2024-12-17 14:41:26 +0800

    change port

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import next from "next";
 import { Server } from "socket.io";
 
 const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 
 const app = next({ dev });
